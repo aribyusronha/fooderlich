e5aeec500f109c7cdabd3f2a352c25e18e600ce1
aribyusronha <arib.yusronhamdani2078@students.unila.ac.id>
2022-10-04 08:37:51 +0700

    Modul Praktikm 5 - Add Models
    Menambahkan Models Yang akan di gunakan

---
+++ lib/models/explore_data.dart
@@ -0,0 +1,11 @@
+import 'models.dart';
+
+class ExploreData{
+    final List<ExploreRecipe> todayRecipes;
+    final List<Post> friendPost;
+
+    ExploreData({
+        this.todayRecipes,
+        this.friendPost,
+    });
+}
---
+++ lib/models/post.dart
@@ -0,0 +1,20 @@
+class Post {
+    String profileImageUrl;
+    String comment;
+    String foodPictureUrl;
+    String timeStamp;
+
+    Post({
+        this.profileImageUrl,
+        this.comment,
+        this.foodPictureUrl,
+        this.timeStamp,
+    });
+
+    Post.fromJSON(Map<String, dynamic> json) {
+        profileImageUrl = json['profileImageUrl'];
+        comment = json['comment'];
+        foodPictureUrl = json['foodPictureUrl'];
+        timeStamp = json['timeStamp'];
+    }
+}
