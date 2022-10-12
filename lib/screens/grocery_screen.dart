9be7b55848703d6aafbfa5e2b25e7dbcd9192876
aribyusronha <arib.yusronhamdani2078@students.unila.ac.id>
2022-10-13 01:14:14 +0700

    Modul Praktikum 6 - Add Grocery Screen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,14 +1,38 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:fooderlich/models/models.dart';
-import 'empty_grocery_screen.dart';
+import 'package:fooderlich/screens/empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget{
-    const GroceryScreen ({Key? key}) : super(key: key);
-    
-    @override
-    Widget build(BuildContext context) {
-        return const EmptyGroceryScreen();
+  const GroceryScreen({super.key});
+
+    Widget buildGroceryScreen(){
+        return Consumer<GroceryManager>(
+            builder: (context, manager, child) {
+                if (manager.groceryItems.isNotEmpty){
+                return Container();
+                }
+                else {
+                return const EmptyGroceryScreen();
+                }
+            }
+        );
     }
 
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+                
+            },
+            child: const Icon(Icons.add),
+        ),
+        body: buildGroceryScreen(),
+    );
+  }  
 }
